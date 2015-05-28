41e9c7e9b66cd05bd8d9dd28f0d6012a82f7fedc
Daniel Chen <Maveri2k@yahoo.com>
2015-05-27 23:23:28 -0400

    main.py gets the authors by init Author class in various ways

--- citeseerx_citation_network/main.py
+++ citeseerx_citation_network/main.py
@@ -16,10 +16,26 @@ a.get_page_soup()
 # print(a.soup)
 
 # print(len(a.get_authors_soup()))
-print(a.get_authors().authors)
+print(a.get_authors())
+print(a.authors)
 
 # for blah in a.get_authors_soup():
 #     stripped = blah.strip()
 #     text = re.sub('^by\s+', '', stripped)
 #     cleaned_again = text.strip()
 #     print(cleaned_again)
+
+
+a = article.Article(doi='10.1.1.13.2424')
+print(a.url)
+a.get_page_soup()
+print(a.get_authors())
+print(a.authors)
+
+url = "http://citeseerx.ist.psu.edu/viewdoc/summary?doi="\
+        "10.1.1.13.2424"
+a = article.Article(url=url)
+print(a.url)
+a.get_page_soup()
+print(a.get_authors())
+print(a.authors)
