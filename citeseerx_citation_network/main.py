120c7ada3eacd04a71dbf89212e47e23a628d762
Daniel Chen <Maveri2k@yahoo.com>
2015-05-28 17:29:35 -0400

    comment out article code

--- citeseerx_citation_network/main.py
+++ citeseerx_citation_network/main.py
@@ -7,41 +7,41 @@ import citations
 imp.reload(article)
 imp.reload(citations)
 
-a = article.Article()
+# a = article.Article()
 
-a.url = "http://citeseerx.ist.psu.edu/viewdoc/summary?doi="\
-        "10.1.1.13.2424"
+# a.url = "http://citeseerx.ist.psu.edu/viewdoc/summary?doi="\
+#         "10.1.1.13.2424"
 
-print(a.url)
+# print(a.url)
 
-a.get_page_soup()
+# a.get_page_soup()
 
-# print(a.soup)
+# # print(a.soup)
 
-# print(len(a.get_authors_soup()))
-print(a.get_authors())
-print(a.authors)
+# # print(len(a.get_authors_soup()))
+# print(a.get_authors())
+# print(a.authors)
 
-# for blah in a.get_authors_soup():
-#     stripped = blah.strip()
-#     text = re.sub('^by\s+', '', stripped)
-#     cleaned_again = text.strip()
-#     print(cleaned_again)
+# # for blah in a.get_authors_soup():
+# #     stripped = blah.strip()
+# #     text = re.sub('^by\s+', '', stripped)
+# #     cleaned_again = text.strip()
+# #     print(cleaned_again)
 
-time.sleep(5)
+# time.sleep(5)
 
-a = article.Article(doi='10.1.1.13.2424')
-print(a.url)
-a.get_page_soup()
-print(a.get_authors())
-print(a.authors)
+# a = article.Article(doi='10.1.1.13.2424')
+# print(a.url)
+# a.get_page_soup()
+# print(a.get_authors())
+# print(a.authors)
 
-time.sleep(3.5)
+# time.sleep(3.5)
 
-url = "http://citeseerx.ist.psu.edu/viewdoc/summary?doi="\
-        "10.1.1.13.2424"
-a = article.Article(url=url)
-print(a.url)
-a.get_page_soup()
-print(a.get_authors())
-print(a.authors)
+# url = "http://citeseerx.ist.psu.edu/viewdoc/summary?doi="\
+#         "10.1.1.13.2424"
+# a = article.Article(url=url)
+# print(a.url)
+# a.get_page_soup()
+# print(a.get_authors())
+# print(a.authors)
