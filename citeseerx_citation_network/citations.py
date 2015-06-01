1f636276525a3231b0e9121928497d5477e6b58c
Daniel Chen <Maveri2k@yahoo.com>
2015-06-01 13:01:47 -0400

    get num results and num page results

--- citeseerx_citation_network/citations.py
+++ citeseerx_citation_network/citations.py
@@ -3,6 +3,7 @@
 """Module that defines citations search results from an article"""
 
 import re
+import math
 
 import requests
 from bs4 import BeautifulSoup
@@ -37,6 +38,46 @@ class Citations(permalink.DigitalObjectIdentifier):
         match = re.match('^Results\s\d+\s-\s\d+\sof\s\d+', single_white_space)
         assert match is not None
         return(single_white_space)
+
+    def get_num_results(self, result_info=None,
+                        split1=' of ', idx1=1,
+                        split2=' ', idx2=0):
+        if result_info is None:
+            result_info = self.result_info
+
+        num_results = result_info.\
+            split(split1)[idx1].\
+            split(split2)[idx2]
+        self.num_results = int(num_results)
+        print(num_results)
+        return(self)
+
+    def get_num_page_results(self, num_results=None, num_results_per_page=10,
+                             offset=1):
+        """Get the number of search page results from a given number of results
+
+        :param num_results: number of results from a search, defaults to None
+        because self.num_results typically needs to be caculated first
+        :type num_results: int
+
+        :param num_results_per_page: number of results per page, default to 10
+        :type num_results_per_page: int
+
+        :param offset: offset value for number of page result calculation.
+        Default is 1.
+        :type offset: int
+
+        The offset value exists because citeseerx does not count by page
+        results, but rather it shows the result number to start from.
+        if the num_results_per_page is 10, then the second page will show
+        10.  This offset is so that we can multiply the num_page_results
+        value by 10, and know which result number will be on the last page.
+        """
+        if num_results is None:
+            num_results = self.num_results
+        self.num_page_results = \
+            math.ceil(num_results / float(num_results_per_page)) - offset
+        return(self)
 
     def get_result_info(self, find_index=0):
         """Get the number of results
